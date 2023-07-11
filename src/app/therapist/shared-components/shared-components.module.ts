806801e7a53139db3f112e085ead0cb5bff42ffa
jordanfvc26 <jverac12@uteq.edu.ec>
2023-07-10 21:29:56 -0500

    Dashboard, componentes de sesiones, tareas, chat

--- src/app/therapist/shared-components/shared-components.module.ts
+++ src/app/therapist/shared-components/shared-components.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InfoLoginComponent } from './info-login/info-login.component';
-import { SliderVideosComponent } from './slider-videos/slider-videos.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InfoLoginComponent } from './info-login/info-login.component';
+import { LastLoginsComponent } from './last-logins/last-logins.component';
+import { TasksToReviewComponent } from './tasks-to-review/tasks-to-review.component';
+import { SliderVideosComponent } from './slider-videos/slider-videos.component';
+import { ChatIAComponent } from './chat-ia/chat-ia.component';
 
 @NgModule({
   declarations: [
     InfoLoginComponent,
-    SliderVideosComponent
+    SliderVideosComponent,
+    LastLoginsComponent,
+    TasksToReviewComponent,
+    ChatIAComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +21,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   exports:[
     InfoLoginComponent,
-    SliderVideosComponent
+    LastLoginsComponent,
+    TasksToReviewComponent,
+    SliderVideosComponent,
+    ChatIAComponent
   ]
 })
 export class SharedComponentsModule { }
