5c5dcdcc7314db91afb4b16d488ce843228898a8
jordanfvc26 <jverac12@uteq.edu.ec>
2023-07-13 13:45:39 -0500

    Tareas y Login, Toasts

--- src/app/therapist/modules/auth/login/login.component.ts
+++ src/app/therapist/modules/auth/login/login.component.ts
@@ -4,6 +4,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/therapist/services/auth.service';
 import * as iconos from '@fortawesome/free-solid-svg-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -17,11 +19,13 @@ export class LoginComponent {
   constructor(
     private ruta: Router,
     private api: AuthService,
-    public modal: NgbModal
+    public modal: NgbModal,
+    private toastr: ToastrService
   ) { }
 
-  ngOnInit(){
-    this.spinnerStatus = false;
+
+  ngOnInit() {
+    this.spinnerStatus = true;
   }
 
   loginForm = new FormGroup({
@@ -32,16 +36,13 @@ export class LoginComponent {
   /*Método que inicia la sesión del usuario*/
   loginUser() {
     this.spinnerStatus = false;
-    let headers = new Map();
-    headers.set("identification", this.loginForm.value.identification);
-    headers.set("password", this.loginForm.value.password);
-
-    this.api.loginUser(headers).subscribe(data => {
+    this.api.loginUser(this.getHeaders()).subscribe(data => {
       sessionStorage.setItem("token", data.token);
       sessionStorage.setItem("role", data.role)
       if (data.role == 'THERAPIST') {
         this.spinnerStatus = true;
         this.ruta.navigateByUrl('/home/dashboard');
+        this.showToast("Inicio de sesión exitoso", "Bienvenido")
       }
       else {
         this.spinnerStatus = true;
@@ -49,17 +50,41 @@ export class LoginComponent {
       }
     }, error => {
       this.spinnerStatus = true;
-      alert("Credenciales incorrectas");
+      this.showToastError("Error", "No se puede inciar sesión")
     })
   }
 
+  /*Obtiene y retorna los headers*/
+  getHeaders(){
+    let headers = new Map();
+    headers.set("identification", this.loginForm.value.identification);
+    headers.set("password", this.loginForm.value.password);
+    return headers;
+  }
+
+  /*Método que muestra un toast con mensaje de ÉXITO*/
+  showToast(message: string, title: string){
+    this.toastr.success(message, title, {
+      progressBar: true,
+      timeOut: 3000,
+    });
+  }
+
+  /*Método que muestra un toast con mensaje de ERROR*/
+  showToastError(title: string, message: string){
+    this.toastr.error(message, title, {
+      progressBar: true,
+      timeOut: 3000,
+    });
+  }
+
   /*Método que redirige al módulo de recuperar la contraseña*/
-  goToForgotPassword(){
+  goToForgotPassword() {
     this.ruta.navigateByUrl('/auth/forgot-password');
   }
 
   /*Método que muestra modal para los usuarios que no tienen una cuenta*/
-  openModalNoAccount(noAccount: any){
+  openModalNoAccount(noAccount: any) {
     this.modal.open(noAccount, { size: 'lg', centered: true });
   }
 
