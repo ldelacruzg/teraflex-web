6c3dc4bb93613f4426bf0e2b837329fae839c8fa
Mauricio Manzaba <mauricio.9.inm@gmail.com>
2023-08-30 23:20:56 -0500

    Configuración para deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
-    production: false,
-    apiUrl: import.meta.env.NG_APP_URL_API
-  };
-  
+  production: true,
+  urlApi: import.meta.env.NG_APP_URL_API,
+  THERAPIST: import.meta.env.NG_APP_THERAPIST,
+  ADMIN: import.meta.env.NG_APP_ADMIN,
+  PATIENT: import.meta.env.NG_APP_PATIENT,
+  TITLE: import.meta.env.NG_APP_TITLE,
+  FIRSTNAME: import.meta.env.NG_APP_FIRSTNAME,
+  CATEGORYNAME: import.meta.env.NG_APP_CATEGORYNAME,
+  ITEMS_FOR_PAGE: import.meta.env.NG_APP_ITEMS_FOR_PAGE,
+  INITIAL_PAGE: import.meta.env.NG_APP_INITIAL_PAGE
+};
